44d1527824ce060437c4aaf4745800ff1d724562
johr07 <johr07@gmail.com>
2018-03-12 03:53:05 +0900

    j

--- js/all.js
+++ js/all.js
@@ -16,11 +16,7 @@ $(function() {
     html += ' <div class="fixed"> ';
     html += ' <h1 class="logo"> ';
     html += ' <span class="text_hide">R의 포트폴리오</span> ';
-    html += ' <div> ';
-    html += ' <svg class="title_logo" enable-background="new 0 0 240 236" version="1.1" viewBox="0 0 300 260" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"> ';
-    html += ' <polygon points="49 236 49 52.2 178.9 236 240 236 240 0 191 0 191 174.7 69.8 0 0 0 0 236" fill="#ffd40a"/> ';
-    html += ' </svg> ';
-    html += ' </div> ';
+    html += ' <div class="title_logo">R</div> ';
     html += ' </h1> ';
     html += ' <div class="menu" role="button" tabindex="0"> ';
     html += ' <div class="menu_burger" tabindex="0"> ';
