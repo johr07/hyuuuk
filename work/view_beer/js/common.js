c73856d9c9d008799721a05baa2de4169b9a062c
johr07 <sqwasd@naver.com>
2018-12-05 00:57:24 +0900

    asd
    asd

--- work/view_beer/js/common.js
+++ work/view_beer/js/common.js
@@ -1,25 +1,3 @@
-function loadMotion() {
-    var $motion = $('.n-motion');
-    var windowT;
-    if ($motion.length) {
-        $motion.each(function (i) {
-            var $this = $(this);
-            var thisT = $this.offset().top;
-            var thisH = $this.height() / 2;
-            var thisP = thisT + thisH;
-            var event = 'load.' + i + ' scroll.' + i;
-
-            $(window).on(event, function () {
-                windowT = $(window).scrollTop() + $(window).outerHeight();
-                if (windowT > thisP) {
-                    $this.addClass('n-active');
-                    $(window).off(event);
-                }
-            });
-        });
-    }
-};
-
 function QuickActive() {
     var $quickBtn = $('.header > .gnb > ul.right > li > a');
     var headerH = $('.header').height();
@@ -83,6 +61,5 @@ function parallaxIt() {
 };
 $(function () {    
     parallaxIt();
-    loadMotion();
     QuickActive();
 });
