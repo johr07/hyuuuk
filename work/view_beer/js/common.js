14323eedcd676b43381726374c89cb008f198c6a
johr07 <sqwasd@naver.com>
2018-12-05 00:24:28 +0900

    dasd
    asd

--- work/view_beer/js/common.js
+++ work/view_beer/js/common.js
@@ -32,6 +32,55 @@ function QuickActive() {
     });
 };
 
+// makes the parallax elements
+function parallaxIt() {
+    // create variables
+    var $fwindow = $(window);
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    var $contents = [];
+    var $backgrounds = [];
+
+    // for each of content parallax element
+    $('[data-type="content"]').each(function (index, e) {
+        var $contentObj = $(this);
+
+        $contentObj.__speed = ($contentObj.data('speed') || 1);
+        $contentObj.__fgOffset = $contentObj.offset().top;
+        $contents.push($contentObj);
+    });
+
+    // for each of background parallax element
+    $('[data-type="background"]').each(function () {
+        var $backgroundObj = $(this);
+
+        $backgroundObj.__speed = ($backgroundObj.data('speed') || 1);
+        $backgroundObj.__fgOffset = $backgroundObj.offset().top;
+        $backgrounds.push($backgroundObj);
+    });
+
+    // update positions
+    $fwindow.on('scroll resize', function () {
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        $contents.forEach(function ($contentObj) {
+            var yPos = $contentObj.__fgOffset - scrollTop / $contentObj.__speed;
+
+            $contentObj.css('top', yPos);
+        })
+
+        $backgrounds.forEach(function ($backgroundObj) {
+            var yPos = ((scrollTop - $backgroundObj.__fgOffset) / $backgroundObj.__speed);
+
+            $backgroundObj.css({
+                backgroundPosition: '50% ' + yPos + 'px'
+            });
+        });
+    });
+
+    // triggers winodw scroll for refresh
+    $fwindow.trigger('scroll');
+};
 function keydowncheck() {
     var result = true;
     var keycode = event.keyCode;
@@ -41,15 +90,6 @@ function keydowncheck() {
     }
     return result;
 }
-
-function parallaxScroll() {
-    var currentScrollTop = $(window).scrollTop();
-    $('.con1 .bg1').css('top', (0 + (currentScrollTop * .1)) + 'px');
-    $('.con1 .bg2').css('top', (0 + (currentScrollTop * .1)) + 'px');
-    $('.con3 .bg1').css('top', (0 + ((currentScrollTop) * .1)) + 'px');
-    $('.con3 .bg2').css('top', (0 + ((currentScrollTop) * .1)) + 'px');
-}
-
 $(function () {
     /* $(document).bind("contextmenu", function(e) {
 		return false;
@@ -64,13 +104,7 @@ $(function () {
         }
     });
     */
-    $(window).scroll(function (e) {
-        parallaxScroll();
-        var ccc = $('.con3 .bg2').offset().top;
-        console.log(ccc);
-    });
-    var ccc = $('.con3 .bg2').offset().top;
-    console.log(ccc);
+    parallaxIt();
     loadMotion();
     QuickActive();
 });
