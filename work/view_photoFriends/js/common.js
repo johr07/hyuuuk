f643bc348675823e13edaae24dedae9c337f0fa7
hyuuuk <36949135+johr07@users.noreply.github.com>
2019-02-25 00:20:37 +0900

    asd
    asd

--- work/view_photoFriends/js/common.js
+++ work/view_photoFriends/js/common.js
@@ -1,18 +1,19 @@
 function openPopup(id, callback) {
     var thisPopup = $('#popup_' + id);
-    $('.popupWrap').fadeIn();
+    // $('.popupWrap').fadeIn();
     $('html, body').addClass('on');
     thisPopup.fadeIn();
     $(document).mouseup(function (e) {
       if (!$('.popup').is(e.target) && $('.popup').has(e.target).length === 0){
-        $('.popupWrap').fadeOut();
-        $('.popup').fadeOut();
+        // $('.popupWrap').fadeOut();
+        // $('.popup').fadeOut();
+        $('html, body').removeClass('on');
       };
     });
   };
   function closePopup(){
-    $('.popupWrap').fadeOut();
-    $('.popup').fadeOut();
+    // $('.popupWrap').fadeOut();
+    // $('.popup').fadeOut();
     $('html, body').removeClass('on');
   };
   
