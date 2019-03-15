cc59988f046fe61f955610c91b4e75d6ed3ad380
hyuuuk <36949135+johr07@users.noreply.github.com>
2019-03-15 15:55:24 +0900

    asd
    asd

--- work/view_planetWorldTour/js/common.js
+++ work/view_planetWorldTour/js/common.js
@@ -2,4 +2,9 @@ $(function () {
     $('.collapsible').collapsible();
     $('.tabs').tabs();
     $('.dropdown-trigger').dropdown();
+
+    $('.dropdown-content a').click(function(){
+        var asd = $(this).html();
+        $(this).parent().parent().parent().children('.dropdown-trigger').html(asd);
+    });
 })
