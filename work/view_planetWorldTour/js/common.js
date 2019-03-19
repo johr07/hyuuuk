663f4842dbc85b44ca6dcb926985446c72485c66
hyuuuk <36949135+johr07@users.noreply.github.com>
2019-03-19 15:52:56 +0900

    asd
    asd

--- work/view_planetWorldTour/js/common.js
+++ work/view_planetWorldTour/js/common.js
@@ -3,14 +3,16 @@ $(function () {
     $('.tabs').tabs();
     $('.dropdown-trigger').dropdown();
 
-    $('.dropdown-content a').click(function(){
+    $('.dropdown-content a').click(function () {
         var asd = $(this).html();
         $(this).parent().parent().parent().children('.dropdown-trigger').html(asd);
     });
-    $('.btn_menu').click(function(){
+    $('.btn_open').click(function () {
         $('.header').addClass('on');
+        $('html, body').addClass('on');
     });
-    $('.btn_close').click(function(){
+    $('.btn_close').click(function () {
         $('.header').removeClass('on');
+        $('html, body').removwClass('on');
     });
 })
