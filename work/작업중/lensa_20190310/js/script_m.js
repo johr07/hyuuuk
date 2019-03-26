aef70fc6e61463df1fbc91a856c3d2d0e761bc28
hyuuuk <36949135+johr07@users.noreply.github.com>
2019-03-26 11:55:58 +0900

    asd
    asd

--- work/작업중/lensa_20190310/js/script_m.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $(".btn_open").click(function(){
-        $("header").addClass("on");
-    });
-    $(".btn_close").click(function(){
-        $("header").removeClass("on");
-    });
-});
