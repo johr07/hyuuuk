b3f5b8d167da62821c17421bc03c660ccc22241a
hyuuuk <36949135+johr07@users.noreply.github.com>
2019-04-08 17:03:32 +0900

    asd
    asd

--- work/spready/common/js/common.js
+++ work/spready/common/js/common.js
@@ -2,12 +2,12 @@ $(function () {});
 
 function openPopup(id, callback) {
     var thisPopup = $('#popup-' + id);
-    thisPopup.after('<div class="popup_overlay" onclick="closePopup();"></div>')
+    thisPopup.after('<div class="popup_overlay""></div>')
     thisPopup.fadeIn(200);
     $('body').addClass('popup_open');
     $('.popup_overlay').fadeIn(100);
     $(document).mouseup(function (e) {
-        if (!$('.popup_inner').is(e.target) && $('.popup_inner').has(e.target).length === 0) {
+        if (!$('.popup_inner').is(e.target)) {
             $('.popup').fadeOut(100);
             $('.popup_overlay').fadeOut(200, function () {
                 $('.popup_overlay').remove();
